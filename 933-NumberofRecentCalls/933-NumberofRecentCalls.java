615dbc7d42a3b1b77cb31be873e3b6656c1f60d7
Rahul Gautam <142567312+RAHUL7687686868@users.noreply.github.com>
2025-08-17 23:34:38 +0530

    File updated at 8/17/2025, 11:34:34 PM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.java
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.java
@@ -1,20 +1,6 @@
-// Last updated: 8/17/2025, 11:29:18 PM
+// Last updated: 8/17/2025, 11:34:34 PM
 class Solution {
-    public int arrayNesting(int[] nums) {
-        int maxLen = 0;
-        boolean [] visit = new boolean[nums.length];
-        for(int i  = 0; i < nums.length; i++){
-            if(!visit[i]){
-                int count = 0;
-                int curr = i;
-                while(!visit[curr]){
-                    visit[curr] = true;
-                    curr = nums[curr];
-                    count++;
-                }
-                maxLen = Math.max(maxLen, count);
-            }
-        }
-        return maxLen;
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;    
     }
 }
