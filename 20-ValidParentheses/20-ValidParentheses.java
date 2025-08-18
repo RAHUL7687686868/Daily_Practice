7b7b46f66c4c300981697a515d8f1c8d58834978
Rahul Gautam <142567312+RAHUL7687686868@users.noreply.github.com>
2025-08-18 14:01:21 +0530

    File updated at 8/18/2025, 2:01:15 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,6 +1,8 @@
-// Last updated: 8/18/2025, 1:59:30 PM
+// Last updated: 8/18/2025, 2:01:15 PM
 class Solution {
-    public String toLowerCase(String s) {
-        return s.toLowerCase();
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        String doubled = s + s;
+        return doubled.contains(goal);
     }
 }
