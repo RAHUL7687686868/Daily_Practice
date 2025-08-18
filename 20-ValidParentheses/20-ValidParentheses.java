fdd1dc083b8f43f1a91473b6138467c9fecc33c0
Rahul Gautam <142567312+RAHUL7687686868@users.noreply.github.com>
2025-08-18 13:57:53 +0530

    File updated at 8/18/2025, 1:57:47 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,13 +1,19 @@
-// Last updated: 8/18/2025, 1:56:26 PM
+// Last updated: 8/18/2025, 1:57:47 PM
 class Solution {
-    public char findTheDifference(String s, String t) {
-        int result = 0;
-        for (char c : s.toCharArray()) {
-            result ^= c;
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                answer.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                answer.add("Fizz");
+            } else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else {
+                answer.add(String.valueOf(i));
+            }
         }
-        for (char c : t.toCharArray()) {
-            result ^= c;
-        }
-        return (char) result;
+        return answer;
     }
 }
