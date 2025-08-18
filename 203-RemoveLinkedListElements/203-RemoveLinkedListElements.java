f12a8dab65a3ebb83e32afbf7e7d4be6da964f06
Rahul Gautam <142567312+RAHUL7687686868@users.noreply.github.com>
2025-08-18 14:12:10 +0530

    File updated at 8/18/2025, 2:12:05 PM

--- 203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
@@ -1,23 +1,31 @@
-// Last updated: 8/18/2025, 2:08:29 PM
-class Solution {
-    public boolean isHappy(int n) {
-        int slow = n;
-        int fast = getNext(n);
-
-        while (slow != fast) {
-            slow = getNext(slow);
-            fast = getNext(getNext(fast));
+// Last updated: 8/18/2025, 2:12:05 PM
+public class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+        int zeros = 0;
+        int i = 0;
+        while (i + zeros < n) {
+            if (arr[i] == 0) {
+                if (i + zeros == n - 1) {
+                    arr[n - 1] = 0; 
+                    n -= 1;
+                    break;
+                }
+                zeros++;
+            }
+            i++;
         }
 
-        return slow == 1;
-    }
-    private int getNext(int n) {
-        int totalSum = 0;
-        while (n > 0) {
-            int d = n % 10;
-            totalSum += d * d;
-            n /= 10;
+        int last = i - 1;
+        int writePos = n - 1;
+        while (writePos >= 0) {
+            arr[writePos] = arr[last];
+            writePos--;
+            if (arr[last] == 0) {
+                arr[writePos] = 0;
+                writePos--;
+            }
+            last--;
         }
-        return totalSum;
     }
 }
